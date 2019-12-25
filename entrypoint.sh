99ba55a08daa87fadcd68a8125c68f5c8647369f
sumit singh <sumit.singh4613@gmail.com>
2019-12-25 15:16:59 +0530

    same changes in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -eu
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
-chmod 600 "$SSH_PATH/deploy_key"
+chmod 400 "$SSH_PATH/deploy_key"
 
 
 # Do deployment
